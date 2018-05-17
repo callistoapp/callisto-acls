8e2292d2ad9dcb8e461d672cf1c413407e1cf9d4
cmondion <clement.mondion@clicrdv.com>
2018-05-17 17:17:49 +0200

    feat(app): initial conf for app

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import { buildSchema } from 'graphql'
+import Graphql from './graphql'
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+    res.send('Coucou')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: Graphql.schema,
+    rootValue: Graphql.root,
+    graphiql: true,
+}));
 
 export default app
